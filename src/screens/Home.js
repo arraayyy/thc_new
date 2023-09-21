4ec57888362e50abc11924bc9b25f1b981ba07c6
MarvinNavarro01 <18103694@usc.edu.ph>
2023-09-21 22:14:30 +0800

    home page added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -13,14 +13,32 @@ const Home = () => {
     <SafeAreaView>
         <Header height={80}/>
         <View style={{alignItems: 'center'}}>
+            <View style={styles.greetingOuterContainer}>
+                <View style={styles.greetingInnerContainer}>
+                    <Text 
+                        numberOfLines={1} 
+                        style={styles.greetingText}>Hello ...</Text>
+                    <Text 
+                        numberOfLines={2} 
+                        style={styles.greetingText}>Welcome To Talamban Health Connect Access Our Services</Text>
+                </View>
+            </View>
             <View style={styles.container} numColumns={2}>
                 <TouchableOpacity
-                onPress={() => props.navigation.navigate("Home")}
-                style={styles.profileContainer}>
-                    <View style={styles.profileIconContainer}>
+                onPress={() => {}}
+                style={styles.homeContainer}>
+                    <View>
+                    <Icon style={styles.icon} name='briefcase-medical' size={15} color='#E0E2E1' />
+                    </View>
+                    <Text numberOfLines={1} style={styles.iconName}>SERVICES</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => {}}
+                style={styles.homeContainer}>
+                    <View>
                     <Icon style={styles.icon} name='user-alt' size={15} color='#E0E2E1' />
                     </View>
-                    <Text numberOfLines={1} style={styles.iconName}></Text>
+                    <Text numberOfLines={1} style={styles.iconName}>SERVICES</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -30,10 +48,45 @@ const Home = () => {
 
 export default Home
 
+const width = Dimensions.get('window').width -40;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+    greetingOuterContainer:{
+        marginTop:20,
+        paddingTop: 20,
+        borderTopWidth: 0.5,
+        borderColor: '#000'
+    },  
+    greetingInnerContainer:{
+        width:width-50,
+        backgroundColor: "#9ED5C5",
+        padding: 20,
+        borderRadius: 20,
+    },
+    greetingText:{
+        color: '#FFF'
+    },
+    container: { 
+        paddingTop:50,
+        width: width,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+      },
+      homeContainer: {
+        paddingTop: 5,
+        backgroundColor: '#E6EAE6',
+        width: 130,
+        height: 110,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      icon:{
+        fontSize: 65,
+        color: '#9ED5C5'
+      },
+      iconName:{
+        paddingVertical:5,
+        color: '#9ED5C5',
+        fontWeight: 'bold'
+      }
 })
