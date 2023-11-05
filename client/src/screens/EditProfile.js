d2ce04051cd91509eb28dae81dce7851956b767c
RA <75052820+arraayyy@users.noreply.github.com>
2023-11-05 15:42:42 +0800

    Update EditProfile.js

--- client/src/screens/EditProfile.js
+++ client/src/screens/EditProfile.js
@@ -293,6 +293,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     marginTop: 20,
+    marginBottom: 20,
   },
   backButton: {
     backgroundColor: 'white',
